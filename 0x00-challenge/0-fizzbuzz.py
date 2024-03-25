d24dd4bee01ac25c0972771aea497f216d3069a1
Salifabu <otuemobeabubakar@gmail.com>
2024-03-24 18:04:05 -0700

    Fix_My_Code_Challenge

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -29,10 +29,8 @@ def fizzbuzz(n):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print("Missing number")
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
         print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
     number = int(sys.argv[1])
